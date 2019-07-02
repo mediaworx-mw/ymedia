46ba4d44e10140523b53d8a2308c4751dcc7241c
Genaro Desia Coppola <genarodesia@gmail.com>
2019-07-02 11:57:32 -0300

    fix para que aparezcan los graficos en todos los layouts

--- single-canal.php
+++ single-canal.php
@@ -58,6 +58,11 @@
             <div class="canal-single__entry">
               <?php get_template_part('components/entry-canal'); ?>
             </div>
+            <div class="canal-single__graficos entry">
+              <?php the_field('graficos_canal'); ?>
+
+              <?php require get_template_directory() . '/inc/graficos-newsletters/master.php'; ?>             
+            </div>
           </div>
         </div>
 
@@ -104,8 +109,10 @@
             <div class="canal-single__entry">
               <?php get_template_part('components/entry-canal'); ?>
             </div>
-             <div class="canal-single__graficos entry">
+            <div class="canal-single__graficos entry">
               <?php the_field('graficos_canal'); ?>
+
+              <?php require get_template_directory() . '/inc/graficos-newsletters/master.php'; ?>             
             </div>
           </div>
         </div>
@@ -156,8 +163,7 @@
             <div class="canal-single__graficos entry">
               <?php the_field('graficos_canal'); ?>
 
-              <?php require get_template_directory() . '/inc/graficos-newsletters/master.php'; ?>
-                                  
+              <?php require get_template_directory() . '/inc/graficos-newsletters/master.php'; ?>             
             </div>
           </div>
         </div>
