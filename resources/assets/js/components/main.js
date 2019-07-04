697622f02bcf61d8dd191e7586e67a90e1c3f595
Eduardo Plaza <edoplaza@gmail.com>
2019-07-04 18:20:46 +0200

    mouse trail 2

--- resources/assets/js/components/main.js
+++ resources/assets/js/components/main.js
@@ -36,25 +36,72 @@ const Main = () => {
 //     });
 // }, false);
 
-  $(function() {
+  // $(function() {
+  //   (function($) {
+  //     let addPoint = function(pageX, pageY) {
+  //       let point = $("<div>", {"class": 'cursor-trail', css: {left: pageX, top: pageY }}).appendTo('body');
+  //       point.animate( { opacity: 1 }, 2000, () => point.remove() )
+  //     };
+  //     $.fn.cursorTrail = function() {
+  //       return this.bind("mousemove", function(ev) {
+  //         addPoint(ev.pageX, ev.pageY);
+  //       });
+  //     };
+  //   }($));
+  //   $("#trail").cursorTrail();
+  // });
 
-    (function($) {
-      let addPoint = function(pageX, pageY) {
-        let point = $("<div>", {"class": 'cursor-trail', css: {left: pageX, top: pageY }}).appendTo('body');
-        point.animate( { opacity: 1 }, 2000, () => point.remove() )
-      };
 
-      $.fn.cursorTrail = function() {
-        return this.bind("mousemove", function(ev) {
-          addPoint(ev.pageX, ev.pageY);
-        });
-      };
-    }($));
+  let dots = [];
+  let mouse = {x: 0,y: 0};
 
-    $("#trail").cursorTrail();
+  let Dot = function() {
+    this.x = 0;
+    this.y = 0;
+    this.node = (function(){
+      let n = document.createElement("div");
+      n.className = "trail";
+      document.body.appendChild(n);
+      return n;
+    }());
+  };
 
+  Dot.prototype.draw = function() {
+    this.node.style.left = this.x + "px";
+    this.node.style.top = this.y + "px";
+  };
+
+  for (let i = 0; i < 12; i++) {
+    let d = new Dot();
+    dots.push(d);
+  }
+
+  function draw() {
+    let x = mouse.x;
+    let y = mouse.y;
+
+    dots.forEach(function(dot, index, dots) {
+
+      let nextDot = dots[index + 1] || dots[0];
+      dot.x = x;
+      dot.y = y;
+      dot.draw();
+      x += (nextDot.x - dot.x) * .6;
+      y += (nextDot.y - dot.y) * .6;
+    });
+  }
+
+  addEventListener("mousemove", function(event) {
+    mouse.x = event.pageX;
+    mouse.y = event.pageY;
   });
 
+  function animate() {
+    draw();
+    requestAnimationFrame(animate);
+  }
+
+  animate();
 }
 
 export default Main;
