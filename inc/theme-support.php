6b071f63569fe30dcfff2214a40980881945c579
Eduardo Plaza <edoplaza@gmail.com>
2019-08-20 19:15:12 +0200

    twitter share

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -240,8 +240,9 @@ function queryParameters($data) {
 function share_buttons() {
   global $post;
   $postURL = get_permalink($post->ID);
-  $postTitle = str_replace( ' ', '%20', get_the_title($post->ID));
-  $twitterURL = 'https://twitter.com/intent/tweet?text='.$postTitle.'&amp;via=&amp;hashtags=&amp;url='.$postURL;
+  //$postTitle = str_replace( ' ', '%20', get_the_title($post->ID));
+  $postTitle = urlencode(html_entity_decode(get_the_title(), ENT_COMPAT, 'UTF-8'));
+  $twitterURL = 'https://twitter.com/intent/tweet?text='.$postTitle.'&amp;via=ymedia.es&amp;url='.$postURL;
   $facebookURL = 'https://www.facebook.com/sharer/sharer.php?u='.$postURL;
   $emailURL = 'mailto:?Subject=' .$postTitle.'&amp;Body=Sharing%20with%20you:%20'.$postURL;
   $social = '';
